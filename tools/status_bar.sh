e3c9662a0a6b77350ddd30c715f7412f5d3218e4
bb <user@l08it>
2014-09-04 16:02:04 +0200

    restructured vimrc

--- tools/status_bar.sh
+++ tools/status_bar.sh
@@ -10,8 +10,6 @@ readonly PRE='{ "full_text": "'
 readonly COL='", "color": "'
 readonly CER='" }'
 
-KERNELV=$PRE"K: $(uname -sr)"$COL'#bbaacc'$CER
-
 i=5
 
 while :
@@ -20,10 +18,10 @@ do
   if [ "$i" -eq "5" ]; then 
     BATERIA=$PRE"B: $(acpi -b | awk '{print $NF}')"$COL'#ccbbaa'$CER
     DISK=$PRE"D: $(df -h | awk '/cr_home/ {print $5 " ("$4" Disp.)" }')"$COL'#ccaabb'$CER
-    let i=0
+    i=0
   fi
 
-  let i="$i+1"
+  i="$i+1"
 
   LOAD=$PRE"L: $(cat /proc/loadavg)"$COL'#ccbbaa'$CER
   VOL=$PRE"♪: $(pactl list sinks | awk '/Volume: 0:/{print $NF}')"$COL'#bbccaa'$CER
